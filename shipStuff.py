eb57bf47c57eaffd9af6986e36165511bac83023
jazzyj22 <40178596+jazzyj22@users.noreply.github.com>
2018-07-23 17:07:54 -0500

    asdhdasj

---
+++ shipStuff.py
@@ -0,0 +1,26 @@
+xCoord = 550 
+yCoord = 550 
+xSpeed = 10
+ySpeed = 10
+ellipseSize = 30 
+
+def flyingShip(): 
+
+    background(0, 0, 175) 
+    global xCoord, yCoord,y, xSpeed, ySpeed, ellipseSize 
+    leftBoundary = ellipseSize / 2 
+    rightBoundary = 400 - ellipseSize / 2 
+    topBoundary = 540 - ellipseSize / 2  
+    bottomBoundary = 600 - ellipseSize /2 
+    xCoord += xSpeed 
+    yCoord += ySpeed 
+
+    if mouseX >= rightBoundary or mouseX<= leftBoundary: 
+        xSpeed = -xSpeed  
+    if yCoord >= bottomBoundary or yCoord<= topBoundary: 
+        ySpeed = -ySpeed
+    fill(255, 255, 255) 
+    shipY = mouseY        #top boundary for ship
+    if shipY < 420: 
+        shipY =  420
+    ellipse(mouseX, shipY, ellipseSize, ellipseSize)
