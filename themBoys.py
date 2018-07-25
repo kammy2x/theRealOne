ef8dfc4a3ee0d18a08e34156f2f34e23807b0e07
Masaniai18 <MythofAhkilion@gmail.com>
2018-07-25 14:46:19 -0500

    df

--- themBoys.py
+++ themBoys.py
@@ -1,4 +1,3 @@
-
 def themBoys():
     fill(155)
     triangle(30, 75, 58, 20, 86, 75) #1stship
@@ -19,6 +18,7 @@ def themBoys():
     triangle(300, 75, 328, 20, 356, 75) #4thship
     fill(185,0,255)
     ellipse(328, 50, 15, 40)
+    
     fill(155)
     triangle(80, 165, 108, 105, 136, 165) #5thship
     fill(0, 185, 255)
