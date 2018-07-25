3ac3a273e8b54c081f68a1da800f29d60d0a26ac
kammy2x <40400159+kammy2x@users.noreply.github.com>
2018-07-25 11:43:28 -0500

    added in 3 more ships, changed color, to give them some pazaaazz

--- themBoys.py
+++ themBoys.py
@@ -13,10 +13,26 @@ def themBoys():
     fill(155)
     triangle(210,75, 238, 20, 266, 75) #3rdship
     fill(185,0,255)
-    ellipse(238, 50, 14, 40)
+    ellipse(238, 50, 15, 40)
     
     fill(155)
     triangle(300, 75, 328, 20, 356, 75) #4thship
     fill(185,0,255)
     ellipse(328, 50, 15, 40)
+    fill(155)
+    triangle(80, 165, 108, 105, 136, 165) #5thship
+    fill(0, 185, 255)
+    ellipse(108, 135, 15, 40)
+    
+    fill(155)
+    triangle(170, 165, 198, 105, 226, 165) #6thship
+    fill(0, 185, 255)
+    ellipse(198, 135, 15, 40)
+    
+    fill(155)
+    triangle(260, 165, 288, 105, 316, 165) #7thship
+    fill(0, 185, 255)
+    ellipse(288, 135, 15, 40)
+    
+    
    
