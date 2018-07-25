d6fec5d0616e955269435c554cd2cec24128a93f
jazzyj22 <40178596+jazzyj22@users.noreply.github.com>
2018-07-25 11:41:08 -0500

    All 4 ships dissappear!

--- LoLo.py
+++ LoLo.py
@@ -1,6 +1,8 @@
 laserY = 75 
 shotShip1 = False 
-shotShip2 = False
+shotShip2 = False 
+shotShip3 = False 
+shotShip4 = False 
 def begin():
     #size(400,600)
     fill (30,90,100)
@@ -25,20 +27,41 @@ def laser():
          line(mouseX,mouseY,mouseX,laserY)
          
 def shotShips():
-    global laserY, shotShip1 
+    global laserY, shotShip1, shotShip2, shotShip3, shotShip4 
     
-    if mousePressed and mouseX >=30 and mouseX <=86: #Conditional to make ships disappear
+    if mousePressed and mouseX >=30 and mouseX <=86: #Conditional to make the first ship disappear
         shotShip1 = True  
         fill (0) 
         triangle(30, 75, 58, 20, 86, 75)
     if shotShip1 == True:                            #Conditional to keep the ship "gone"
         fill(0) 
         triangle(30, 75, 58, 20, 86, 75) 
+    if mousePressed and mouseX >= 120 and mouseX <= 176: #Conditional to make the second ship disappear
+        shotShip2 = True 
+        fill(255, 0, 0)
+        triangle( 120, 75, 148, 20, 176, 75) 
+    if shotShip2 == True:                                #Conditional to keep the ship out of view
+        fill(0) 
+        triangle( 120, 75, 148, 20, 176, 75)  
+    if mousePressed and mouseX >= 210 and mouseX <= 266: 
+        shotShip3= True
+        fill(255, 0, 0) 
+        triangle(210,75, 238, 20, 266, 75)
+    if shotShip3 == True: 
+        fill(0) 
+        triangle(210, 75, 238, 20, 266, 75) 
+    if mousePressed and mouseX >=300 and mouseX<=356: 
+        shotShip4 = True
+        fill(255, 0, 0) 
+        triangle(300, 75, 328, 20, 356, 75) 
+    if shotShip4 == True: 
+        fill(0) 
+        triangle(300, 75, 328, 20, 356, 75)
 
-        
 #def shooter(): 
     #laserX = 30 
     #laserY = 75
     #if laserX >=30 and laserY <= 75: 
         #fill(0) 
         #triangle(30, 75, 58, 20, 86, 75)
+        
